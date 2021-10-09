c815c59cc8756b0cba92a664617aa3e0ff2ab601
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-10-09 18:07:55 +0530

    changes made

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-const startBtn = document.querySelector(".play")
+/*const startBtn = document.querySelector(".play")
 const stopBtn = document.querySelector(".stop")
-const hole = document.querySelector(".holes").children
+const hole = document.querySelector(".holes")
 
 console.log(startBtn, hole)
 
-const startGame = setInterval(()=>{
+startBtn.addEventListener("click", ()=>{
 
 
     const arr = [0,1,2, 3, 4, 5, 6, 7, 8]
     const index = 4// store index using random function
-    console.log(hole[index])
-
+    console.log(hole.children[index])
+/*
     const image = document.createElement("img")
     image.src = "images/mole.png"
     image.setAttribute("class", "mole")
     
-    hole[index].appendChild(image)
+    hole.children[index].appendChild(image)
     setTimeout(()=> {
 
         hole[index].removeChild(image)
     }, 900)
-}, 1000)
-startBtn.addEventListener("click", ()=> {
-
-    startBtn.style.display = none;
-    stopBtn.style.display = visible;
-
-    startGame
-})
+    
+} )
+*/
