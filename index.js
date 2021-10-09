72f4063a788ae278228c12ef95bac84f0f733eed
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-10-09 20:47:35 +0530

    changes made

--- index.js
+++ index.js
@@ -2,43 +2,58 @@ const startBtn = document.querySelector(".play")
 const stopBtn = document.querySelector(".stop")
 const hole = document.querySelector(".holes")
 const cursor = document.querySelector(".hammer-cursor img")
+const score = document.querySelector(".score")
 
 console.log(startBtn, hole)
 let prev = -1
 
-window.addEventListener("mousemove", (e)=>{
+window.addEventListener("mousemove", (e) => {
 
     cursor.style.top = e.pageY + "px"
     cursor.style.left = e.pageX + "px"
 
-   
-    
 })
-window.addEventListener("click", ()=> {
+window.addEventListener("click", () => {
 
     cursor.style.animation = "hit 0.2s ease"
 
-    setTimeout( () => cursor.style.removeProperty("animation"), 1000)
+    setTimeout(() => cursor.style.removeProperty("animation"), 1000)
 })
+let num = 0
+startBtn.addEventListener("click", () => {
 
-startBtn.addEventListener("click", () =>{ setInterval(()=>{
-
-    const index = Math.floor(Math.random()*9)
+    startBtn.style.display = "none"
+    stopBtn.style.display = "inline-block"
     
+    const startGame = setInterval(() => {
+
+        const index = Math.floor(Math.random() * 9)
+
+        if (prev != index) {
+
+            const image = document.createElement("img")
+            image.src = "images/mole.png"
+            image.setAttribute("class", "mole")
+
+            hole.children[index].appendChild(image)
+            setTimeout(() => {
+
+                hole.children[index].removeChild(image)
+            }, 800)
+        }
+        prev = index;
+        console.log(hole.children[index])
+
+    }, 1000)
+
+    stopBtn.addEventListener("click",  ()=> {
+
+        startBtn.style.display = "inline-block"
+        stopBtn.style.display = "none"
+        clearInterval(startGame)
+    })
+
     // store index using random function
-    if(prev != index) {
 
-        const image = document.createElement("img")
-    image.src = "images/mole.png"
-    image.setAttribute("class", "mole")
-    
-    hole.children[index].appendChild(image)
-    setTimeout(()=> {
 
-        hole.children[index].removeChild(image)
-    }, 800)
-    }
-    prev = index;
-    console.log(hole.children[index])
-    
-}, 1000)})
+})
